cef473d9873e0cec374fa4b0000bda8e7efec518
Shadowfax224 <sahilshahpatel@gmail.com>
2019-02-28 22:28:56 -0600

    Creating initial files

---
+++ rfid_check/rfid_check.ino
@@ -0,0 +1,14 @@
+#include <EEPROM.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal.h>
+
+void setup() {
+  // intialiaze pins
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
