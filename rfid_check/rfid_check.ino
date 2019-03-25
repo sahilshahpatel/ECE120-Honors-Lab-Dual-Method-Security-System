dbc72de11a241a4919e529472f48ad9bda7f43ca
Shadowfax224 <sahilshahpatel@gmail.com>
2019-03-25 15:59:45 -0500

    Fixed compilation errors

--- rfid_check/rfid_check.ino
+++ rfid_check/rfid_check.ino
@@ -15,10 +15,21 @@
 #define redLed 30 // Password Denied
 #define greenLed 31 // Password Accepted
 #define blueLed 32 // Standby
+#define clockPin 10
+#define inputPassPin 11
+#define accptPassPin 12
+#define passAcceptedPin 19 // Output of comparator circuit
+#define passComparisonDone 20 // Async 'done' signal of comparator circuit
+
+// Define EEPROM addresses
+#define INPUT_PASSWORD_BASE_ADDR 0
+#define ACCEPTABLE_PASSWORD_BASE_ADDR 5
+#define PASSWORD_LENGTH 4
 
 // Define program variables
 boolean match = false;
-uint8_t successRead; // TODO: is this necessary?
+byte currentPasswordAddr = 0;
+const int clockDelay = 10;
 
 // Set up MFRC522 RFID instance
 #define SS_PIN 40
@@ -26,7 +37,7 @@ uint8_t successRead; // TODO: is this necessary?
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 // Set up LCD
-LiquidCrystal lcd(/*TODO: set LCD pins*/);
+LiquidCrystal lcd = LiquidCrystal(1, 2, 3, 4, 5, 6/*TODO: set LCD pins*/);
 
 // Set up 4x3 Matrix Keypad
 const byte ROWS = 4;
@@ -40,29 +51,32 @@ const byte keys[ROWS][COLS] = {
 };
 const byte rowPins[ROWS] = {10, 11, 12, 13};
 const byte colPins[COLS] = {14, 15, 16};
-Adafuit_Keypad keypad = Adafuit_Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Adafruit_Keypad keypad = Adafruit_Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   // intialiaze pinModes
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
   pinMode(blueLed, OUTPUT);
-
+  pinMode(inputPassPin, OUTPUT);
+  pinMode(accptPassPin, OUTPUT);
+  pinMode(passAcceptedPin, INPUT);
+  
   // Set initial variable states
   reset();
 
   // Initialize communications
   Serial.begin(9600); // Serial communication with computer
   SPI.begin(); // for MFRC522
-  mfrc522.PDC_Init();
+  mfrc522.PCD_Init();
   lcd.begin(16, 2);
   keypad.begin();
 
   // Set RFID read range to max
-  mrfc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
 }
 
-void loop() {
+void loop() {  
   // Read from keypad
   keypad.tick();
 
@@ -73,17 +87,44 @@ void loop() {
       Serial.println(" pressed");
       // Check if # was pressed
       if ((int)e.bit.KEY == keys[ROWS][COLS]){
-        // TODO: output recorded data serially
+        byte inputPassword[PASSWORD_LENGTH];
+        int clockDelay = 100;
+        for(int i = 0; i < PASSWORD_LENGTH; i++){
+          inputPassword[i] = EEPROM.read(INPUT_PASSWORD_BASE_ADDR + i);
+        }
+        outputToComparator(inputPassword, PASSWORD_LENGTH, clockDelay);
+        // If comparator has accepted the password...
+        while(digitalRead(passComparisonDone) == LOW){
+            // Wait for passComparisonDone to be HIGH
+        }
+        if(digitalRead(passAcceptedPin) == HIGH){
+          granted(10);
+        }
+        else{
+          denied(10);
+        }
+        
+        currentPasswordAddr = 0;
       }
       // Check if * was pressed
       else if ((int)e.bit.KEY == keys[ROWS][0]){
-        // TODO: let user reset their password
+        /** TODO: let user reset their password
+         *  Have this go through the same process as if they pressed #,
+         *  but this time if the password is accepted, prompt them to enter a new
+         *  password within X seconds that will replace their old one.
+         */
       }
       else{
-        // TODO: record the pressed button into EEPROM
+        // Record the pressed button into EEPROM
+        if(currentPasswordAddr < INPUT_PASSWORD_BASE_ADDR + PASSWORD_LENGTH){        
+          EEPROM.write(INPUT_PASSWORD_BASE_ADDR + currentPasswordAddr, (int)e.bit.KEY);
+          currentPasswordAddr += 1;
+        }
       }
     }
-    else if(e.bit.EVENT == KEY_JUST_RELEASED) Serial.println(" released");
+    else if(e.bit.EVENT == KEY_JUST_RELEASED){
+      Serial.println(" released");
+    }
   }
 
   // Read pass
@@ -92,20 +133,21 @@ void loop() {
 }
 
 // Helper methods to define other behaviors
-reset(){
+void reset(){
   // Set LEDs to standby
   digitalWrite(redLed, LED_OFF);
   digitalWrite(greenLed, LED_OFF);
   digitalWrite(blueLed, LED_ON);
 
   // TODO: clear LCD screen
+  // TODO: set comparator done signal LOW
 }
 
 void granted(uint16_t setDelay){
   digitalWrite(redLed, LED_OFF);
   digitalWrite(greenLed, LED_ON);
   digitalWrite(blueLed, LED_OFF);
-  delay(setDelay)
+  delay(setDelay);
   reset();
 }
 
@@ -117,24 +159,20 @@ void denied(uint16_t setDelay){
   reset();
 }
 
-// From rfid library example:
-uint8_t getID() {
-   // Getting ready for Reading PICCs
-  if ( ! mfrc522.PICC_IsNewCardPresent()) { // If a new PICC placed to RFID reader continue
-    return 0;
+void outputToComparator(byte pass[], int passLength, int clockDelay){
+  // Iterate over password length
+  for(int i = 0; i < passLength; i++){
+    // Iterate over bits per byte
+    for(int j = 0; j < 8; j++){
+      // Output passwords for comparison
+      digitalWrite(inputPassPin, bitRead(pass[i], j));
+      digitalWrite(accptPassPin, bitRead(pass[i], j));
+
+      // Output clock cycle with specified period
+      digitalWrite(clockPin, HIGH);
+      delay(clockDelay/2);
+      digitalWrite(clockPin, LOW);
+      delay(clockDelay/2);
+    }
   }
-  if ( ! mfrc522.PICC_ReadCardSerial()) {   // Since a PICC placed get Serial and continue
-    return 0;
-  }
-  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
-  // I think we should assume every PICC as they have 4 byte UID
-  // Until we support 7 byte PICCs
-  // Serial.println(F("Scanned PICC's UID:"));
-  for ( uint8_t i = 0; i < 4; i++) {  //
-    readCard[i] = mfrc522.uid.uidByte[i];
-    Serial.print(readCard[i], HEX);
-  }
-  Serial.println("");
-  mfrc522.PICC_HaltA(); // Stop reading
-  return 1;
 }
